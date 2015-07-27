901e7c5872de6c6fb61a315327a3532b1494b0bc
Tres Seaver <tseaver@palladion.com>
2015-07-27 15:56:15 -0400

    Work around Travis.

--- setup.py
+++ setup.py
@@ -21,11 +21,14 @@ from setuptools import find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(here, 'README.rst')) as f:
-    README = f.read()
+try:
+    with open(os.path.join(here, 'README.rst')) as f:
+        README = f.read()
 
-with open(os.path.join(here, 'CHANGES.rst')) as f:
-    CHANGES = f.read()
+    with open(os.path.join(here, 'CHANGES.rst')) as f:
+        CHANGES = f.read()
+except IOError:
+    README = CHANGES = ''
 
 testing_extras = ['nose', 'coverage']
 
